b55ad85621b5e4dbf11c3d90edbb725d243d2168
Robert Chandler <robert@akchandler.com>
2024-03-22 17:12:10 -0700

    Fix everything

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
 import { Detail, LaunchProps } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 
-import { bcv_parser, ParsedEntity } from "bible-passage-reference-parser/js/en_bcv_parser";
+type BibleApiVerse = {
+  book_id: string;
+  book_name: string;
+  chapter: number;
+  verse: number;
+  text: string;
+}
 
-const bcv = new bcv_parser();
+type BibleAPIResult = {
+  reference: string;
+  text: string;
+  translation_name: string;
+  verses: BibleApiVerse[];
+}
+
 
 export default function Command(props: LaunchProps<{ arguments: Arguments.Index }>) {
   const reference = props.arguments.reference;
-  const parsed = bcv.parse(reference).parsed_entities()[0].entities[0] as ParsedEntity;
-  const { b, c, v } = parsed.start;
-  const url = `https://bible-api.com/${b}-${c}:${v}`;
+  const url = `https://bible-api.com/${reference}`
   const { isLoading, data } = useFetch(url);
 
-  // json encode the data
-  console.log(JSON.stringify(data, null, 2));
-
-  return <Detail isLoading={isLoading} markdown={data.text} />;
+  let content = "";
+  if (data) {
+    const d = data as unknown as BibleAPIResult;
+    for (const v of d.verses) {
+      content += ` *${v.verse}* ${v.text}`
+    }
+    content += `\n\n# ${d.reference}\n\n## ${d.translation_name}`
+  }
+  return <Detail
+    isLoading={isLoading}
+    markdown={content}
+  />;
 }
